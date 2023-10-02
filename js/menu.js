4ac2badc0e5264f8655fe93182877222fd5864f4
nrmattar <nrmattar@gmail.com>
2023-10-02 20:41:34 -0300

    20231002

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+/*
+function changeDivValue() {
+    const menu = document.querySelector(".menu");
+    const display = getComputedStyle(menu).display;
+
+    if (window.innerWidth <= 600) {
+        if (display == "none") {
+            menu.style = "display: flex; flex-direction: column; row-gap: 2vh;"
+            const main = document.querySelector("#main")
+            main.style = "margin-top: 45vh;"
+        }
+        else {
+            menu.style = "display: none; "
+            const main = document.querySelector("#main")
+            main.style = "margin-top: 0vh;"
+        }
+    }
+}
+*/
